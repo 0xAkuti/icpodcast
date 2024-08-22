5e92b9b11700cd31dd9088be913547b9b0c4708b
Akuti <40723201+0xAkuti@users.noreply.github.com>
2024-08-22 12:37:12 +0800

    Initial commit

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
-import { backend } from 'declarations/backend';
 
 function App() {
   const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
+  function handleSubmit(event: any) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
+    fetch(`${import.meta.env.VITE_CANISTER_URL}/greet?name=${name}`)
+      .then(response => response.json()).then((json) => {
+        setGreeting(json.greeting)
+      });
   }
 
   return (
@@ -24,7 +23,7 @@ function App() {
         <button type="submit">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
-    </main>
+    </main >
   );
 }
 
